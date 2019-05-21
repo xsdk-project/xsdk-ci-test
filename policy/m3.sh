b7f07a20c2237a374ff84fab531433b4bc4bf66e
Perrinel Mériadeg <meriadeg.perrinel@ng-analytics.com>
2019-05-21 17:39:50 +0200

    Update m3 test policy

--- policy/m3.sh
+++ policy/m3.sh
@@ -3,7 +3,7 @@
 # This script aims to test the m3 policy on a package
 # The first parameter must be an inner package source
 
-echo "Tests of XSDK m1 policy ..."
+echo "Tests of XSDK m3 policy ..."
 
 PKGSOURCE=$1
 
@@ -13,21 +13,34 @@ if [ ! -d $PKGSOURCE ]; then
     exit 1
 fi
 
-# m1 test 3.1: Check if the source code contains any occurence of MPI_COMM_WORLD
-counter=0
-for file in $PKGSOURCE/*
-do
-    if [ -f $file ]; then
+# loop & print a folder recusively,
+count_mpi_occurence() {
+for i in "$1"/*;do
+    if [ -d "$i" ];then
+        count_mpi_occurence "$i"
+    elif [ -f "$i" ]; then
         if grep -q MPI_COMM_WORLD "$file"; then
-            counter=$((counter+1))
+            $2=$(($2+1))
         fi
     fi
 done
+}
+
+# m1 test 3.1: Check if the source code contains any occurence of MPI_COMM_WORLD
+counter=0
+count_mpi_occurence $PKGSOURCE $counter
+#for file in $PKGSOURCE/*
+#do
+#    if [ -f $file ]; then
+#        if grep -q MPI_COMM_WORLD "$file"; then
+#            counter=$((counter+1))
+#        fi
+#    fi
+#done
 
 echo "MPI_COMM_WORLD has been found $counter times in the sources code "
-if counter > 0; then
+if [ $counter -gt 0 ]; then
     echo "Test 3.1 : Checking MPI_COMM_WORLD is not used in the source code: Failure"
-fi
 else
     echo "Test 3.1 : Checking MPI_COMM_WORLD is not used in the source code: Succes"
 fi
